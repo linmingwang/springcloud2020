f6c188334cb71c0360e3b3c715e3030630210428
v_linmingwang <964280219@qq.comgit commit -m first>
2020-08-26 17:13:14 +0800

    提交

--- cloud-provider-payment8004/src/main/java/com/lmw/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8004/src/main/java/com/lmw/springcloud/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.lmw.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -11,6 +12,7 @@ public class PaymentController {
     @Value("${server.port}")
     private String port;
 
+    @GetMapping("/payment/zk")
     public String paymentZk () {
         return "springcloud with zookeeper:"+port+"\t"+ UUID.randomUUID().toString();
     }
